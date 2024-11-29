12012950f41b6d1a7cd3ecbf624f8b21c2d43188
yashwanth123456-789 <yashwanthkumar.dandigola@slu.edu>
2024-11-29 03:40:51 +0000

    Wee6 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import productData from './data/full-products';
+import CardList from './components/CardList';
+import Header from './components/Header';
 
+import productData from './data/full-products';
+import { Route, Routes} from 'react-router-dom';
+import SingleView from './components/SingleView';
 function App() {
   return (
     <div className="App">
-      <h1>Hello There.</h1>
+      <Header />
+      <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
     </div>
   );
 }
